b6191dfa85d6a7c64eeaa935eb8d9cb8e99d07a7
Tommy <tab114@pitt.edu>
2016-09-28 02:45:50 -0400

    Added walking skeleton and gradle buildfile

---
+++ src/main/java/laboon/Chess.java
@@ -0,0 +1,13 @@
+package laboon;
+
+public class Chess {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static String poodle() {
+        return "poodle";
+    }
+
+}
---
+++ src/test/java/laboon/ChessTest.java
@@ -0,0 +1,14 @@
+package laboon;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import laboon.Chess;
+
+public class ChessTest {
+
+    @Test
+    public void testPoodle() {
+        assertEquals(Chess.poodle(), "poodle");
+    }
+}
